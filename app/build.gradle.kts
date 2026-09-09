d70620d90d56cc5a64c7ce4b2aad88c0991c461c
Daddelgreis <47208852+Daddelgreis74@users.noreply.github.com>
2026-09-09 09:18:46 +0200

    Bump version to v2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.smarthomekiosk"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.5"
+        versionCode = 6
+        versionName = "2.6"
     }
 
     signingConfigs {
